1142153d1fb71676f621f8586a39a82af27984b9
yongjiezhang54-web <yongjiezhang54@gmail.com>
2026-06-03 16:39:06 +0800

    Delete src directory

--- src/app/records/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <main className="mx-auto max-w-md px-4 pb-20 pt-6">
-      <div className="mb-6 h-7 w-24 animate-pulse rounded bg-gray-200" />
-      <div className="h-72 animate-pulse rounded-2xl bg-gray-100" />
-    </main>
-  );
-}
